fc1bdc9c99e44616412191db570746147d2cfd6d
José Valverde <delfimvalverde@gmail.com>
2016-11-25 16:25:32 +0000

    First Commit
    With tutorial I2C code

---
+++ Debug/ee24c16.d
@@ -0,0 +1,5 @@
+ee24c16.o ee24c16.o: ../ee24c16.c ../ee24c16.h ../twi.h
+
+../ee24c16.h:
+
+../twi.h:
---
+++ Debug/i2ceeprom.d
@@ -0,0 +1,7 @@
+i2ceeprom.o i2ceeprom.o: ../i2ceeprom.c ../usart.h ../ee24c16.h ../twi.h
+
+../usart.h:
+
+../ee24c16.h:
+
+../twi.h:
